b71d18f428e71a9fea015bacda3e1aaf522f4bb7
vecpeng <841298391@qq.com>
2020-08-05 21:47:22 +0800

    85

---
+++ C/Project/pages/search/search.js
@@ -0,0 +1,12 @@
+new Vue({
+    el:"#container",
+    data:{
+        hotSearch:["建筑史概论","建筑史","建筑史","建筑史","建筑史","建筑史"],
+        searchHistory:["建筑史概论","建筑史概论"]
+    },
+    methods:{
+        deleteHistory:function(index){
+           this.searchHistory = this.searchHistory.splice(index,1)
+        }
+    }
+})
